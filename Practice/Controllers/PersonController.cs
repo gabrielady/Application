0e69907aae1a05f34be66f50d3c31f2cc169bf23
Adilene <fadilene65@gmail.com>
2019-07-22 16:15:14 -0500

    Project New

---
+++ Practice/Controllers/PersonController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice.Models;
+
+namespace Practice.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            PersonModel person = new PersonModel();
+            person.ID = 2;
+            person.Name = "Adilene";
+            person.LName = "Fernadez";
+            person.Age = 28;
+            person.Address = "Colima";
+            
+            return View(person);
+        }
+
+        public ActionResult Content (int Personid)
+        {
+
+            return Content(string.Format("ID:{0}", Personid));
+       
+        }
+
+        public ActionResult EmptyResult()
+        {
+            return new EmptyResult();
+        }
+
+        public ActionResult ActionResult()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult ViewResult ()
+        {
+            return ViewResult();
+        }
+
+        // GET: Person/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Person/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Person/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Person/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Person/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Practice/Models/PersonModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practice.Models
+{
+    public class PersonModel
+    {
+        public int ID { get; set; }
+        public string Name{ get; set; }
+        public string LName { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+
+    }
+}
